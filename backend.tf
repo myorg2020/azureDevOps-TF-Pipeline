25f363524d87ac755b91f0a3c019c220c2e5f65c
arops2020 <amitesh1258@gmail.com>
2024-06-16 23:14:11 +0530

    Update RG name in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
+    resource_group_name  = "demo-rg"
     storage_account_name = "tfstatestrgacc"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
